394aa22c2ba98950c53069caee376d786258fd37
Claudenes Santos <claudenes-santos@proton.me>
2026-02-11 13:15:28 -0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.[contenthash].js',
+    filename: 'bundle.[contenthash].js',           
     clean: true,
   },
   module: {
